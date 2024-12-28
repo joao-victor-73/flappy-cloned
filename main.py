76992eb13323133840f1febb69f496177ff75b11
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2024-12-27 21:49:52 -0300

    criando o joguinho do flappy (3)

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ class Passaro:
         self.tempo = 0
         self.contagem_imagem = 0 # servirá para saber qual a imagem do passaro (bird1, bird2 ou bird3)
         
-        self.imagem = IMGS[0]
+        self.imagem = self.IMGS[0]
 
     def pular(self):
         self.velocidade = -10.5
@@ -88,9 +88,63 @@ class Passaro:
             if self.angulo > -90:
                 self.angulo -= self.VELOCIDADE_ROTACAO
 
+    def desenhar(self, tela):
+        # definir qual imagem do passaro vai usar
+        self.contagem_imagem += 1
+
+        if self.contagem_imagem < self.TEMPO_ANIMACAO:
+            self.imagem = self.IMGS[0]
+
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO:
+            self.imagem = self.IMGS[1]
+
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO:
+            self.imagem = self.IMGS[2]
+
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*4:
+            self.imagem = self.IMGS[1]
+
+        elif self.contagem_imagem >= self.TEMPO_ANIMACAO*4 + 1:
+            self.imagem = self.IMGS[0]
+
+        # Se o passaro tiver caindo, eu não vou bater asa
+        if self.angulo <= -80:
+            self.imagem = self.IMGS[1]
+            self.contagem_imagem = self.TEMPO_ANIMACAO*2
+
+        # Desenhar a imagem
+        imagem_rotacionada = pygame.transform.rotate(self.imagem, self.angulo)
+        pos_centro_imagem = self.imagem.get_rect(topleft=(self.x, self.y)).center
+        retangulo = imagem_rotacionada.get_rect(center=pos_centro_imagem)
+        tela.blit(imagem_rotacionada, retangulo.topleft)
+
+    def get_mask(self):
+        pygame.mask.from_surface(self.imagem)
 
 class Cano:
-    pass
+    # Essa distância é entre o cano de cima e o de baixo
+    # E esse valor de 200 é em px
+    DISTANCIA = 200 
+    VELOCIDADE = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.altura = 0
+        self.pos_topo = 0
+        self.pos_base = 0
+        
+        self.CANO_TOPO = pygame.transform.flip(IMAGEM_CANO, False, True)
+        self.CANO_BASE = IMAGEM_CANO
+
+        self.passou = False
+
+        self.definir_altura()
+
+    def definir_altura(self):
+        self.altura = random.randrange(50, 450)
+        self.pos_topo = self.altura - self.CANO_TOPO.get_height()
+        self.pos_base = self.altura + self.DISTANCIA
+
 
 class Chao:
     pass
